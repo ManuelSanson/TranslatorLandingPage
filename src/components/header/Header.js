ab16d51ee79f9b4a5291f5d733bd495f2c6e4156
Manuel Sanson <manu.sanson9@gmail.com>
2022-10-14 14:48:25 -0300

    title and logo as link

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 
@@ -11,13 +12,13 @@ const Header = () => {
 
     return (
         <header className='header'>
-            <div className='headerTextLogoContainer'>
-                <img src="" alt='MS Traducciones Logo'/>
-                <h1> MS Traducciones </h1>
-            </div>
+            <Link to='/' className='headerTextLogoContainer'> 
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/%C3%86toms_-_Translation.svg/800px-%C3%86toms_-_Translation.svg.png" alt='MS Traducciones Logo'/>
+                <h1> MS Traducciones </h1>       
+            </Link>
             <div className='languageButtonsContainer'>
-                <button onClick={() => changeLanguage('en')}> <img src='https://res.cloudinary.com/diqry3ihg/image/upload/c_scale,w_81/v1665611223/TranslatorLandingPage/US-UKFlag_msslku.png' alt='US-UK Flag'/>  English</button>
-                <button onClick={() => changeLanguage('esp')}> <img src='https://res.cloudinary.com/diqry3ihg/image/upload/c_scale,h_56,w_81/v1665611223/TranslatorLandingPage/SpainFlag_bbhqmj.png' alt='Spanish Flag'/> Español</button>
+                <button onClick={() => changeLanguage('en')}> <img src='https://res.cloudinary.com/diqry3ihg/image/upload/c_scale,w_81/v1665611223/TranslatorLandingPage/US-UKFlag_msslku.png' alt='US-UK Flag'/> <p>English</p> </button>
+                <button onClick={() => changeLanguage('esp')}> <img src='https://res.cloudinary.com/diqry3ihg/image/upload/c_scale,h_56,w_81/v1665611223/TranslatorLandingPage/SpainFlag_bbhqmj.png' alt='Spanish Flag'/> <p>Español</p> </button>
             </div>
         </header>
     )
