046b3492d2dd4fd05ac57538e7b183c2eece194c
Manuel Sanson <manu.sanson9@gmail.com>
2022-10-14 13:33:15 -0300

    responsive

--- src/components/home/introduction/Introduction.js
+++ src/components/home/introduction/Introduction.js
@@ -1,10 +1,13 @@
 import { Button, Card, Container } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import './Introduction.css';
 
 const Introduction = () => {
     const { t } = useTranslation();
+    AOS.init()
     return (
         <section className="introductionSection">
             <Container className="pt-2">
@@ -23,18 +26,18 @@ const Introduction = () => {
                     </div>
                 </article>
                 <article className="guarantees">
-                    <Card className='bg-light align-items-center text-center' style={{ width: '28rem' }}>
+                    <Card data-aos={"fade-right"} data-aos-duration={"800"} className='guaranteeCard bg-light align-items-center text-center m-1 mb-3'>
                         <Card.Img variant="top" src="https://thedenverdentists.com/wp-content/uploads/2019/01/Checkmark-circle-blue.png" className="cardImage"/>
                         <Card.Body>
-                            <Card.Title> <h3> {t('Quality')} </h3> </Card.Title>
-                            <Card.Text> {t('QualityText')} </Card.Text>
+                            <Card.Title className="cardTitle"> <h3> {t('Quality')} </h3> </Card.Title>
+                            <Card.Text className="cardText"> {t('QualityText')} </Card.Text>
                         </Card.Body>
                     </Card>
-                    <Card className='bg-light align-items-center text-center' style={{ width: '28rem' }}>
+                    <Card data-aos={"fade-left"} data-aos-duration={"800"} className='guaranteeCard bg-light align-items-center text-center m-1'>
                         <Card.Img variant="top" src="https://www.shareicon.net/data/512x512/2017/06/21/887416_clock_512x512.png" className="cardImage"/>
                         <Card.Body className="justify-content-center">
-                            <Card.Title> <h3> {t('Speed')} </h3> </Card.Title>
-                            <Card.Text> {t('SpeedText')} </Card.Text>
+                            <Card.Title className="cardTitle"> <h3> {t('Speed')} </h3> </Card.Title>
+                            <Card.Text className="cardText"> {t('SpeedText')} </Card.Text>
                         </Card.Body>
                     </Card>
                 </article>
