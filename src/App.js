5e79327c6405d4ea57ec8bdc91d6c7dafa5d7c21
Manuel Sanson <manu.sanson9@gmail.com>
2022-10-19 20:12:19 -0300

    create contact form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 import Header from './components/header/Header';
 import NavBar from './components/navbar/NavBar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -10,10 +9,10 @@ import Footer from './components/footer/Footer';
 import AboutMe from './components/aboutMe/AboutMe';
 import WAppButton from './components/wappButton/WAppButton';
 import Services from './components/services/Services';
+import Contact from './components/contact/Contact';
 
 
 function App () {
-  const { t } = useTranslation();
 
   return (
     <BrowserRouter>
@@ -24,7 +23,7 @@ function App () {
         <Route path='/' element={<Home/>} />
         <Route path='/aboutme' element={<AboutMe/>} />
         <Route path='/services' element={<Services/>} />
-        <Route path='/contact' element={<h1>{t('Contact')}</h1>} />
+        <Route path='/contact' element={<Contact/>} />
       </Routes>
       <Footer/>
     </BrowserRouter>
---
+++ src/components/contact/Contact.js
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import { Button, Container, Form } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
+import Swal from "sweetalert2";
+import './Contact.css';
+
+
+//VER RESPONSIVE Y ADONDE ENVIAR MENSAJE
+const Contact = () => {
+    const { t } = useTranslation();
+
+    const [emailAddress, setEmailAddress] = useState('');
+    const [subject, setSubject] = useState('');
+    const [message, setMessage] = useState('');
+
+    const submitMessage = (e) => {
+        e.preventDefault();
+        if (emailAddress !== '' && subject !== '' && message !== '') {
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: `${t('MessageSent')}`,
+                showConfirmButton: false,
+                timer: 1500
+            }) 
+            setEmailAddress('');
+            setSubject('');
+            setMessage('');
+        } else {
+            Swal.fire({
+                position: 'center',
+                icon: 'error',
+                title: `${t('FillAll')}`,
+                showConfirmButton: false,
+                timer: 1500
+            })
+        }
+    }
+
+    return (
+        <main>
+            <h2 className="text-center mt-4 mb-4"> {t('Help')} </h2>
+            <h3 className="m-4"> {t('SendMessage')} </h3>
+            <Container className="formContainer mb-4">
+                <Form className="mt-4 contactForm">
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Label> {t('EmailAddress')} </Form.Label>
+                        <Form.Control value={emailAddress} type="email" placeholder="name@example.com" onChange={(e) => setEmailAddress(e.target.value)}/>
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Label> {t('Subject')} </Form.Label>
+                        <Form.Control type="text" value={subject} onChange={(e) => setSubject(e.target.value)}/>
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                        <Form.Label> {t('Message')} </Form.Label>
+                        <Form.Control as="textarea" rows={3} value={message} onChange={(e) => setMessage(e.target.value)}/>
+                    </Form.Group>
+                </Form>
+            </Container>
+            <div className="mb-5 text-center">
+                <Button size='lg' onClick={submitMessage} variant="success" type="submit">
+                    {t('Submit')}
+                </Button>
+            </div>
+        </main>
+    )
+}
+
+export default Contact;
