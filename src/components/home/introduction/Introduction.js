c45e694f625adb7bd27372866edd5b4f0e87a1e1
Manuel Sanson <manu.sanson9@gmail.com>
2022-11-02 21:42:20 -0300

    change button color and guarantees img

--- src/components/home/introduction/Introduction.js
+++ src/components/home/introduction/Introduction.js
@@ -15,26 +15,26 @@ const Introduction = () => {
                     <h2> {t('Welcome')} </h2>
                     <p> {t('IntroductionText')} </p>
                     <div className='text-center'>
-                        <Button variant='light' className="border" as={Link} to='aboutme'> {t('ReadMore')} </Button>
+                        <Button variant='light' className="border button" as={Link} to='aboutme'> {t('ReadMore')} </Button>
                     </div>
                 </article>
                 <article className="introductionArticles">
                     <h3> {t('Services')} </h3>
                     <p> {t('ServicesIntroductionText')} </p>
                     <div className='text-center'>
-                        <Button variant='light' className="border" as={Link} to='services'> {t('ReadMore')} </Button>
+                        <Button variant='light' className="border button" as={Link} to='services'> {t('SeeServices')} </Button>
                     </div>
                 </article>
                 <article className="guarantees">
                     <Card data-aos={"flip-up"} data-aos-duration={"800"} className='guaranteeCard bg-light align-items-center text-center m-1 mb-3'>
-                        <Card.Img variant="top" src="https://thedenverdentists.com/wp-content/uploads/2019/01/Checkmark-circle-blue.png" className="cardImage"/>
+                        <Card.Img variant="top" src="https://res.cloudinary.com/diqry3ihg/image/upload/v1667425554/TranslatorLandingPage/verlogo_wg5qxu.png" className="cardImage"/>
                         <Card.Body>
                             <Card.Title className="cardTitle"> <h3> {t('Quality')} </h3> </Card.Title>
                             <Card.Text className="cardText"> {t('QualityText')} </Card.Text>
                         </Card.Body>
                     </Card>
                     <Card data-aos={"flip-up"} data-aos-duration={"800"} className='guaranteeCard bg-light align-items-center text-center m-1 mb-3'>
-                        <Card.Img variant="top" src="https://www.shareicon.net/data/512x512/2017/06/21/887416_clock_512x512.png" className="cardImage"/>
+                        <Card.Img variant="top" src="https://res.cloudinary.com/diqry3ihg/image/upload/v1667426131/TranslatorLandingPage/clockLogo_wfwn8k.png" className="cardImage"/>
                         <Card.Body className="justify-content-center">
                             <Card.Title className="cardTitle"> <h3> {t('Speed')} </h3> </Card.Title>
                             <Card.Text className="cardText"> {t('SpeedText')} </Card.Text>
